b277e22660e4e016105c48a838015be3a33a9382
Brian <ashe1990@hotmail.com>
2023-04-22 17:10:40 -0600

    indexChange

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use(require("./routes/index"));
+app.use(require("./src/routes/index"));
 
 app.listen(process.env.PORT || 8888, () => {
   console.log("Server running");
