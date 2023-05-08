21ac985632ebc8414a291da90c4968d89bd63362
BrianHpLap <ashe1990@hotmail.com>
2023-05-08 09:48:52 -0600

    VersionFunctionalArduino

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require("express");
-const helmet = require("helmet");
-const env = require("./src/env");
-const corsConfig = require("./src/corsConfig");
-
 const app = express();
-
-/* //? CORS
-app.use(corsConfig());
-
-//? Enable raw body on request object
-const rawBodySaver = (req, res, buf, encoding) => {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || "utf8");
-  }
-};
-
-const options = {
-  verify: rawBodySaver
-};
-
-app.use(express.json(options));
-app.use(helmet());
-
-app.set("trust proxy", true); */
+app.use(express.json());
 
 app.use(require("./src/routes/index"));
 
-app.listen(env.PORT, () => {
+//server run
+app.listen(process.env.PORT || 8888, () => {
   console.log("Server running");
 });
 
