8240566b0d87d2f930cec480aad3aaff66a05dda
BrianHpLap <ashe1990@hotmail.com>
2023-05-08 10:26:29 -0600

    corsFunctional

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const helmet = require("helmet");
+const env = require("./src/env");
+const corsConfig = require("./src/corsConfig");
+
 const app = express();
-app.use(express.json());
+
+//? CORS
+app.use(corsConfig(env.NODE_ENV));
+
+//? Enable raw body on request object
+const rawBodySaver = (req, res, buf, encoding) => {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || "utf8");
+  }
+};
+
+const options = {
+  verify: rawBodySaver
+};
+
+app.use(express.json(options));
+app.use(helmet());
+
+app.set("trust proxy", true);
+
+//* Access log
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "../access.log"),
+  { flags: "a" }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(require("./src/routes/index"));
 
-//server run
-app.listen(process.env.PORT || 8888, () => {
+app.listen(env.PORT, () => {
   console.log("Server running");
 });
 
