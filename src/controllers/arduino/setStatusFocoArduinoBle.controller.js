306f6421babe7ce50d2a2ff79ce3cea965cd01f9
BrianHpLap <ashe1990@hotmail.com>
2023-05-31 10:17:10 -0600

    add route ble

---
+++ src/controllers/arduino/setStatusFocoArduinoBle.controller.js
@@ -0,0 +1,33 @@
+const { request, response } = require('express');
+const setStatusFocoArduino = require('../../services/web/setStatusFocoArduino.service');
+const infoFocoArduinoHelpers = require('../../helpers/infoFocoArduino.helpers');
+
+/**
+* @param {request} req - HTTP Request instance from express
+* @param {response} res - HTTP Response instance from express
+* @returns HTTP Response
+*/
+const setStatusFocoArduinoBle= async (req = request, res = response) => {
+  try {
+
+    const setFoco = req.body.data;
+
+    infoFocoArduinoHelpers.setData(setFoco);
+
+    return res.status(200).json({
+      ok: true,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      ok: false,
+      response: {
+        data: null,
+        message: "[server-error]",
+        redirect: false
+      }
+    });
+  }
+}
+
+module.exports = setStatusFocoArduinoBle;
--- src/routes/arduino/arduino.js
+++ src/routes/arduino/arduino.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const sendStatusFocoArduino = require("../../controllers/arduino/sendStatusFocoArduino.controller");
+const setStatusFocoArduinoBle = require("../../controllers/arduino/setStatusFocoArduinoBle.controller");
 
 const router = Router();
 
 router.post("/send/status", sendStatusFocoArduino)
+
+router.post("send/Blestatus", setStatusFocoArduinoBle)
 
 
 module.exports = router;
