b2378c5c8aa3b21f3694eda97c0642f166066aba
BrianHpLap <ashe1990@hotmail.com>
2023-06-02 10:45:28 -0600

    fixError

--- src/services/movil/setStatusFocoArduino.service.js
+++ src/services/movil/setStatusFocoArduino.service.js
@@ -3,7 +3,7 @@ const axios= require("axios");
 
 const setStatusFocoArduino= async (setFoco)=>{
   
-  await axios.post('https://4ad4-200-68-164-39.ngrok-free.app/getLight', {  setFoco });
+  await axios.get('https://4ad4-200-68-164-39.ngrok-free.app/getLight');
   
   return {
     ok: true,
